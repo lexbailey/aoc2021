dd003728bdab724f6b810327500adcd29e0b5aea
Lex Bailey <danieljabailey@gmail.com>
2021-12-13 22:52:09 +0000

    day 3 finished

--- src/day3.rs
+++ src/day3.rs
@@ -24,51 +24,36 @@ pub fn part1(input: &[u8]) -> i64 {
     return gamma_v * sigma_v;
 }
 
-fn filter_lines(lines: &[String], bit: usize, mode: i64) -> Box<[String]>{
-    let mut first = true;
-    let mut width = 0;
-    let mut sums: Vec<i64> = vec![0;0];
-    for l in lines.into_iter() {
-        if first{
-            first = false;
-            width = l.len();
-            sums = vec![0; width];
-        }
-        for n in 0..width {
-            let l = l.as_bytes();
-            sums[n] += if l[n as usize] == '0' as u8 {-1} else {1};
-            print!("{} ", sums[n]);
-        }
-        print!("\n");
-    }
-    let gamma: String = sums.into_iter().map(|x| if x<0{'0'}else{'1'}).collect();
-    let sigma: String = gamma.chars().map(|x| if x=='0'{'1'}else{'0'}).collect();
-    let value = if mode==0{gamma}else{sigma};
-    //let result = lines.filter(|a| a[bit] == value[bit]);
-    //value[bit]
-    Box::new([])
-}
-
 #[aoc(day3, part2)]
 pub fn part2(input: &[u8]) -> i64 {
-    let a = str::from_utf8(input).unwrap().trim().lines();
-    let mut first = true;
-    let mut width = 0;
-    let mut sums: Vec<i64> = vec![0;0];
-    for l in a {
-        if first{
-            first = false;
-            width = l.len();
-            sums = vec![0; width];
-        }
-        for n in 0..width {
+    let mut a:Vec<&str> = str::from_utf8(input).unwrap().trim().lines().collect();
+    let mut b:Vec<&str> = a.clone();
+    let width = a[0].len();
+    for n in 0..width {
+        let mut sum_a = 0;
+        let mut sum_b = 0;
+        
+        for l in &a {
             let l = l.as_bytes();
-            sums[n] += if l[n as usize] == '0' as u8 {-1} else {1};
-            print!("{} ", sums[n]);
+            sum_a += if l[n as usize] == '0' as u8 {-1} else {1};
         }
-        print!("\n");
+        for l in &b {
+            let l = l.as_bytes();
+            sum_b += if l[n as usize] == '0' as u8 {-1} else {1};
+        }
+        if a.len() > 1{
+            let keep_a = if sum_a >= 0 {'1'}else{'0'};
+            a = a.into_iter().filter(|s|s.as_bytes()[n]==keep_a as u8).collect();
+        }
+        if b.len() > 1{
+            let keep_b = if sum_b >= 0 {'0'}else{'1'};
+            b = b.into_iter().filter(|s|s.as_bytes()[n]==keep_b as u8).collect();
+        }
     }
-    return 2;
+    let oxy = i64::from_str_radix(a[0], 2).unwrap();
+    let co2 = i64::from_str_radix(b[0], 2).unwrap();
+    print!("oxy: {}, co2: {}\n",oxy, co2);
+    oxy * co2
 }
 
 
