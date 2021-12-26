1de018cfa628dbb662836eacf8f8859a6130cd79
Lex Bailey <danieljabailey@gmail.com>
2021-12-26 23:43:28 +0000

    over-engineering day 24, solved d23p1

---
+++ src/day23.rs
@@ -0,0 +1,256 @@
+use std::str;
+use std::cmp::min;
+
+fn cost(c: u8) -> i64 {
+    match c{
+        0 => 1
+        ,1 => 10
+        ,2 => 100
+        ,3 => 1000
+        ,_ => panic!("unknown type: {}", c)
+    }
+}
+
+fn path_blocked(c: [u8;7], h: [u8;4], t: [u8;4], from: usize, to: usize) -> bool{
+    //if  h[to]!=9 {
+    //    return true;
+    //}
+    match (from, to){
+         (0,0) => c[1]!=9
+        ,(0,1) => c[1]!=9||c[2]!=9
+        ,(0,2) => c[1]!=9||c[2]!=9||c[3]!=9
+        ,(0,3) => c[1]!=9||c[2]!=9||c[3]!=9||c[4]!=9
+
+        ,(1,0) => false
+        ,(1,1) => c[2]!=9
+        ,(1,2) => c[2]!=9||c[3]!=9
+        ,(1,3) => c[2]!=9||c[3]!=9||c[4]!=9
+
+        ,(2,0) => false
+        ,(2,1) => false
+        ,(2,2) => c[3]!=9
+        ,(2,3) => c[3]!=9||c[4]!=9
+
+        ,(3,0) => c[2]!=9
+        ,(3,1) => false
+        ,(3,2) => false
+        ,(3,3) => c[4]!=9
+
+        ,(4,0) => c[3]!=9||c[2]!=9
+        ,(4,1) => c[3]!=9
+        ,(4,2) => false
+        ,(4,3) => false
+
+        ,(5,0) => c[4]!=9||c[3]!=9||c[2]!=9
+        ,(5,1) => c[4]!=9||c[3]!=9
+        ,(5,2) => c[4]!=9
+        ,(5,3) => false
+
+        ,(6,0) => c[5]!=9||c[4]!=9||c[3]!=9||c[2]!=9
+        ,(6,1) => c[5]!=9||c[4]!=9||c[3]!=9
+        ,(6,2) => c[5]!=9||c[4]!=9
+        ,(6,3) => c[5]!=9
+        ,_ => panic!("mystery path")
+    }
+}
+
+fn steps(from: usize, to: usize) -> i64{
+    match (from, to){
+         (0,0) => 3
+        ,(0,1) => 5
+        ,(0,2) => 7
+        ,(0,3) => 9
+
+        ,(1,0) => 2
+        ,(1,1) => 4
+        ,(1,2) => 6
+        ,(1,3) => 8
+
+        ,(2,0) => 2
+        ,(2,1) => 2
+        ,(2,2) => 4
+        ,(2,3) => 6
+
+        ,(3,0) => 4
+        ,(3,1) => 2
+        ,(3,2) => 2
+        ,(3,3) => 4
+
+        ,(4,0) => 6
+        ,(4,1) => 4
+        ,(4,2) => 2
+        ,(4,3) => 2
+
+        ,(5,0) => 8
+        ,(5,1) => 6
+        ,(5,2) => 4
+        ,(5,3) => 2
+
+        ,(6,0) => 9
+        ,(6,1) => 7
+        ,(6,2) => 5
+        ,(6,3) => 3
+        ,_ => panic!("mystery path")
+    }
+}
+
+fn shortest_path(corridor: [u8;7], room_head: [u8;4], room_tail: [u8;4], depth: &String) -> Option<i64>{
+    assert!(
+        (corridor.iter().filter(|c|**c!=9).count() +
+        [room_head, room_tail].iter().flatten().filter(|c|**c!=9).count()) == 8
+    );
+    if corridor == [9;7]{
+        if (0..=3).all(|i|{
+            room_head[i] as usize == i
+            && room_tail[i] as usize == i
+        }) {
+            return Some(0);
+        }
+    }
+    let mut min_cost: Option<i64> = None;
+    let mut did_step = false;
+    for c in 0..=6 {
+        let p = corridor[c];
+        if p != 9{
+            let step_cost = cost(p);
+            let h = p as usize;
+            if room_tail[h] != 9 && room_head[h] != 9 { continue; }
+            if room_tail[h] != 9 && room_tail[h] != p { continue; }
+            if !path_blocked(corridor, room_head, room_tail, c, h) && room_head[h] == 9{
+                let mut new_corridor = corridor;
+                new_corridor[c] = 9;
+                let mut new_head = room_head;
+                let mut new_tail = room_tail;
+                let mut distance = steps(c, h);
+                if new_tail[h] == 9{
+                    new_tail[h] = p;
+                    distance += 1;
+                }
+                else{
+                    new_head[h] = p;
+                }
+                let cost = step_cost * distance;
+                did_step = true;
+                if let Some(subpath) = shortest_path(new_corridor, new_head, new_tail, &(depth.to_owned() + "    ")){
+                    let tot_cost = cost + subpath;
+                    min_cost = min_cost.and_then(|m|Some(min(m, tot_cost))).or(Some(tot_cost))
+                }
+            }
+        }
+    }
+    if did_step {return min_cost;}
+    // No motion _into_ a room available, move out instead
+    for r in 0..=3_u8{
+        if (room_head[r as usize] != 9 && room_head[r as usize] != r) || (room_tail[r as usize] != 9 && room_tail[r as usize] != r) {
+            let mut p = room_head[r as usize];
+            if p == 9{
+                p = room_tail[r as usize];
+            }
+            let p = p;
+            let step_cost = cost(p);
+            for c in 0..=6{
+                if path_blocked(corridor, room_head, room_tail, c, r as usize) { continue; }
+                let mut new_corridor = corridor;
+                if new_corridor[c as usize] != 9{
+                    continue;
+                }
+                new_corridor[c as usize] = p;
+                let mut new_head = room_head;
+                let mut new_tail = room_tail;
+                let mut distance = steps(c as usize, r as usize);
+                if new_head[r as usize] != 9{
+                    new_head[r as usize] = 9;
+                }
+                else{
+                    new_tail[r as usize] = 9;
+                    distance += 1;
+                }
+                let cost = step_cost * distance;
+                if let Some(subpath) = shortest_path(new_corridor, new_head, new_tail, &(depth.to_owned() + "    ")){
+                    let tot_cost = cost + subpath;
+                    min_cost = min_cost.and_then(|m|Some(min(m, tot_cost))).or(Some(tot_cost))
+                }
+            }
+        }
+    }
+    min_cost
+}
+
+#[aoc(day23, part1)]
+pub fn part1(input: &[u8]) -> i64 {
+    let mut corridor = [9;7];
+    let mut room_head = [9;4];
+    let mut room_tail = [9;4];
+    room_head[0] = input[31]-65;
+    room_head[1] = input[33]-65;
+    room_head[2] = input[35]-65;
+    room_head[3] = input[37]-65;
+
+    room_tail[0] = input[45]-65;
+    room_tail[1] = input[47]-65;
+    room_tail[2] = input[49]-65;
+    room_tail[3] = input[51]-65;
+
+    if let Some(a) = shortest_path(corridor, room_head, room_tail, &"".to_owned()) {
+        a
+    }
+    else{
+        -1
+    }
+}
+
+#[aoc(day23, part2)]
+pub fn part2(input: &[u8]) -> i64 {
+    -1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part1, part2};
+
+    #[test]
+    fn test1(){
+        assert_eq!(part1(
+b"#############
+#...........#
+###B#A#C#D###
+  #A#B#C#D#
+  #########
+"
+        ), 46);
+    }
+
+    #[test]
+    fn test2(){
+        assert_eq!(part1(
+b"#############
+#...........#
+###B#A#C#D###
+  #B#A#C#D#
+  #########
+"
+        ), 114);
+    }
+
+    #[test]
+    fn test3(){
+        assert_eq!(part1(
+b"#############
+#...........#
+###B#C#B#D###
+  #A#D#C#A#
+  #########
+"
+        ), 12521);
+    }
+/*
+    #[test]
+    fn test2(){
+        assert_eq!(part1(
+            b"\ndc-end\nHN-start\nstart-kj\ndc-start\ndc-HN\nLN-dc\nHN-end\nkj-sa\nkj-HN\nkj-dc\n"), 19);
+    }
+*/
+}
+
+
+
---
+++ src/day24.rs
@@ -0,0 +1,358 @@
+use std::str;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fmt::Debug;
+use std::cmp::{min,max};
+
+#[derive(Copy,Clone,Debug)]
+enum Val{
+    Reg(usize)
+    ,Num(i64)
+}
+
+impl Display for Val{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self{
+            Val::Reg(0) => write!(f, "w")
+            ,Val::Reg(1) => write!(f, "x")
+            ,Val::Reg(2) => write!(f, "y")
+            ,Val::Reg(3) => write!(f, "z")
+            ,Val::Reg(a) => write!(f, "<??register id:{}?>", a)
+            ,Val::Num(a) => write!(f, "{}", a)
+        }
+    }
+}
+
+#[derive(Copy,Clone)]
+enum Op{
+    Inp(usize)
+    ,Add(usize, Val)
+    ,Mul(usize, Val)
+    ,Div(usize, Val)
+    ,Mod(usize, Val)
+    ,Eql(usize, Val)
+}
+
+impl Display for Op{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        if let Op::Inp(a) = self{
+            write!(f, "inp {}", Val::Reg(*a))
+        }
+        else{
+            match self{
+                Op::Add(a, b) => write!(f, "add {} {}", Val::Reg(*a), b)
+                ,Op::Mul(a, b) => write!(f, "mul {} {}", Val::Reg(*a), b)
+                ,Op::Div(a, b) => write!(f, "div {} {}", Val::Reg(*a), b)
+                ,Op::Mod(a, b) => write!(f, "mod {} {}", Val::Reg(*a), b)
+                ,Op::Eql(a, b) => write!(f, "eql {} {}", Val::Reg(*a), b)
+                ,_ => unreachable!()
+            }
+        }
+    }
+}
+
+fn reg_index(r: &str) -> Option<usize>{
+    let n = r.as_bytes()[0];
+    if n > 100 {// any number between 57 and 118 could go here
+        Some((n-119).into())
+    }
+    else{
+        None
+    }
+}
+
+impl Op{
+    fn from_str(s: &str) -> Op{
+        let mut parts = s.split(" ");
+        let op_name = parts.next().unwrap();
+        let dest = reg_index(parts.next().unwrap()).unwrap();
+        if op_name == "inp"{
+            Op::Inp(dest)
+        }
+        else {
+            let src = parts.next().unwrap();
+            let src = reg_index(src).and_then(|a|Some(Val::Reg(a))).unwrap_or_else(||Val::Num(src.parse::<i64>().unwrap()));
+            match op_name{
+                "add" => Op::Add(dest, src)
+                ,"mul" => Op::Mul(dest, src)
+                ,"div" => Op::Div(dest, src)
+                ,"mod" => Op::Mod(dest, src)
+                ,"eql" => Op::Eql(dest, src)
+                ,_ => panic!("invalid operation")
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct AluState{
+    regs: [i64;4]
+}
+
+fn eval(v: Val, s: &AluState) -> i64{
+    match v {
+        Val::Reg(i) => s.regs[i]
+        ,Val::Num(n) => n
+    }
+}
+
+fn run(instrs: &Vec<Op>, inputs: &mut dyn Iterator<Item=i64>) -> AluState{
+    let mut s = AluState{regs:[0,0,0,0]};
+    let mut inputs = inputs.into_iter();
+    for i in instrs{
+        match i{
+            Op::Inp(dest) => {s.regs[*dest] = inputs.next().unwrap();}
+            Op::Add(dest, src) => {s.regs[*dest] = s.regs[*dest] + eval(*src, &s);}
+            Op::Mul(dest, src) => {s.regs[*dest] = s.regs[*dest] * eval(*src, &s);}
+            Op::Div(dest, src) => {s.regs[*dest] = s.regs[*dest] / eval(*src, &s);}
+            Op::Mod(dest, src) => {s.regs[*dest] = s.regs[*dest] % eval(*src, &s);}
+            Op::Eql(dest, src) => {s.regs[*dest] = if s.regs[*dest] == eval(*src, &s) {1} else {0};}
+        }
+    }
+    s
+}
+
+fn parse_program(input: &str) -> Vec<Op>{
+    input.lines().map(|l|Op::from_str(l)).collect()
+}
+
+#[derive(Clone,PartialEq)]
+enum Expr{
+    Constant(i64)
+    ,Integer(i64)
+    ,Bounded(i64,i64,Box<Expr>)
+    ,Sum(Box<Expr>,Box<Expr>)
+    ,Product(Box<Expr>,Box<Expr>)
+    ,Quotient(Box<Expr>,Box<Expr>)
+    ,Remainder(Box<Expr>,Box<Expr>)
+    ,WasEq(Box<Expr>,Box<Expr>)
+}
+/*
+impl PartialEq for Expr{
+    fn eq(&self, other: &Rhs) -> bool {
+        match (self, other){
+            (Constant(a), Constant(b)) if a == b => true
+            ,(Integer(a), Integer(b)_) => false
+            ,(_, Integer()) => false
+            ,(Bounded(_,_), _) => false
+            ,(_, Bounded(_,_)) => false
+        }
+    }
+}
+*/
+impl Display for Expr{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        use Expr::*;
+        match self{
+            Constant(a) => write!(f, "{}",a)
+            ,Integer(a) => write!(f, "?<{}>",a)
+            ,Bounded(min,max,expr) => write!(f, "{}..{}?<{}>", min, max, expr)
+            ,Sum(a,b) => write!(f, "({} + {})", a, b)
+            ,Product(a,b) => write!(f, "({} * {})", a, b)
+            ,Quotient(a,b) => write!(f, "({} / {})", a, b)
+            ,Remainder(a,b) => write!(f, "({} % {})", a, b)
+            ,WasEq(a,b) => write!(f, "({} == {})", a, b)
+        }
+    }
+}
+
+impl Debug for Expr{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "{}", self)
+    }
+}
+
+impl Debug for Op{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        let s = format!("{}", self);
+        write!(f, "{:10}", s)
+    }
+}
+
+fn simplify(expr: &Expr) -> Expr{
+    use Expr::*;
+    match expr {
+        Bounded(a, b, expr) if a == b => Constant(*a)
+        ,Product(a, b) => match (&**a, &**b){
+            (a, Constant(0)) => Constant(0)
+            ,(a, Constant(1)) => simplify(a)
+            ,(Constant(0), a) => Constant(0)
+            ,(Constant(1), a) => simplify(a)
+            ,(Constant(a), Constant(b)) => Constant(a*b)
+            ,(Bounded(a_min, a_max, bexpr), Constant(b)) => simplify(&Bounded(a_min*b, a_max*b, Box::new(simplify(&Product(bexpr.clone(), Box::new(Constant(*b)))))))
+            ,(Constant(b), Bounded(a_min, a_max, bexpr)) => simplify(&Bounded(a_min*b, a_max*b, Box::new(simplify(&Product(Box::new(Constant(*b)), bexpr.clone())))))
+            ,_=>expr.clone()
+        } 
+        ,Sum(a,b) => match (&**a, &**b){
+            (Constant(a), Constant(b)) => Constant(a+b)
+            ,(Constant(a), Bounded(min, max, bexpr)) => simplify(&Bounded(min+a, max+a, Box::new(simplify(&Sum(Box::new(Constant(*a)), bexpr.clone())))))
+            ,(Bounded(min, max, bexpr), Constant(a)) => simplify(&Bounded(min+a, max+a, Box::new(simplify(&Sum(bexpr.clone(), Box::new(Constant(*a)))))))
+            ,(a, Constant(0)) => simplify(a)
+            ,(Constant(0), a) => simplify(a)
+            ,_=>expr.clone()
+        }
+        ,Quotient(a,b) => match (&**a, &**b){
+            (Constant(a), Constant(b)) => Constant(a/b)
+            ,(Constant(a), Bounded(min, max, bexpr)) => simplify(&Bounded(min/a, max/a, Box::new(simplify(&Quotient(Box::new(Constant(*a)), bexpr.clone())))))
+            ,(Bounded(min, max, bexpr), Constant(a)) => simplify(&Bounded(min/a, max/a, Box::new(simplify(&Quotient(bexpr.clone(), Box::new(Constant(*a)))))))
+            ,(a, Constant(1)) => simplify(a)
+            ,_=>expr.clone()
+        }
+        ,Remainder(a,b) => match (&**a, &**b){
+            (Constant(a), Constant(b)) => Constant(a%b)
+            ,(Bounded(min_, max_, aexpr), Constant(b)) => simplify(&Bounded(if max_ > b {0} else {*min_}, min(*max_,b-1), Box::new(Remainder(aexpr.clone(), Box::new(Constant(*b))))))
+            ,(a, Constant(b)) => simplify(&Bounded(0, b-1, Box::new(Remainder(Box::new(a.clone()), Box::new(Constant(*b))))))
+            ,_=>expr.clone()
+        }
+        ,WasEq(a,b) => match (&**a, &**b){
+            (Bounded(sa, ea, _), Bounded(sb, eb, _)) if ea < sb || sa > eb => Constant(0)
+            ,(Constant(a), Bounded(min, max, _)) if a < min || a > max => Constant(0)
+            ,(Bounded(min, max, _), Constant(a)) if a < min || a > max => Constant(0)
+            ,(Constant(a), Constant(b)) => Constant(if a==b {1} else {0})
+            ,_=>Bounded(0,1,Box::new(expr.clone()))
+        }
+        ,_ => expr.clone()
+    }
+}
+
+fn noop_elim(program: &Vec<Op>, input_bounds: Option<(i64,i64)>) -> Vec<Op>{
+    const START_STATE: Expr = Expr::Constant(0);
+    let mut reg_states = [START_STATE;4];
+    let mut result: Vec<Op> = Vec::new();
+    let expr = |v,regs:&[Expr;4]|{
+        match v{
+            Val::Num(a) => Expr::Constant(a)
+            ,Val::Reg(a) => regs[a].clone()
+        }
+    };
+    let mut next_id = 1000;
+    let mut line = 1;
+    for op in program{
+        let before_states = reg_states.clone();
+        match op{
+            Op::Inp(a) => {
+                if let Some((min,max)) = input_bounds{
+                    reg_states[*a] = Expr::Bounded(min, max, Box::new(Expr::Integer(next_id)));next_id += 1000;
+                }
+                else{
+                    reg_states[*a] = Expr::Integer(next_id);next_id += 1000;
+                }
+            }
+            Op::Add(a, b) => {reg_states[*a] = Expr::Sum(Box::new(reg_states[*a].clone()), Box::new(expr(*b,&reg_states).clone()));}
+            Op::Mul(a, b) => {reg_states[*a] = Expr::Product(Box::new(reg_states[*a].clone()), Box::new(expr(*b,&reg_states).clone()));}
+            Op::Div(a, b) => {reg_states[*a] = Expr::Quotient(Box::new(reg_states[*a].clone()), Box::new(expr(*b,&reg_states).clone()));}
+            Op::Mod(a, b) => {reg_states[*a] = Expr::Remainder(Box::new(reg_states[*a].clone()), Box::new(expr(*b,&reg_states).clone()));}
+            Op::Eql(a, b) => {reg_states[*a] = Expr::WasEq(Box::new(reg_states[*a].clone()), Box::new(expr(*b, &reg_states).clone()))}
+        }
+        for i in 0..=3{
+            reg_states[i] = simplify(&reg_states[i]);
+        }
+        //println!("After executing {:?} register state is: {:?}", op, reg_states);
+        if (0..=3).all(|i|before_states[i] == reg_states[i]){
+            //println!("{} After executing {:?} register state is unchanged", line, op);
+        }
+        else{
+            //println!("{} After executing {:?}: {:?}", line, op, reg_states);
+            println!("{}: {:?}", line, op);
+            line += 1;
+            result.push(op.clone());
+        }
+    }
+    println!("{:?}", reg_states[3]);
+    result
+}
+/*
+fn rev_exec(program, finals:[Option(i64);4]) -> {
+    let mut states = finals;
+    let mut in_n = 0;
+    for op in program.rev(){
+        match op{
+            Op::Inp(a) => {
+                println!("Input end-{} = {}", in_n, states[a]);
+                in_n += 1;
+            }
+            ,Op::Add(a,b) => {
+                match (states[a], states[b]){
+                    (Some(sa), Some(sb)) => {states[a] = Some(sa - sb)}
+                    (None, Some(sb)) => {states[a] = Some(sa - sb)}
+                }
+            }
+        }
+    }
+}
+*/
+#[aoc(day24, part1)]
+pub fn part1(input: &str) -> i64 {
+    let monad = parse_program(input); // monad is a silly name for this thing <_<
+    let monad = noop_elim(&monad, Some((1,9)));
+    //rev_exec(monad);
+    /*
+    let nums = vec![1..9;14];
+    let mut id: i64 = 11111111111111;
+    loop {
+        id = (id-1).to_string().chars().map(|c| if c == '0' {'1'} else {c}).collect::<String>().parse::<i64>().unwrap();
+        let sid = id.to_string();
+        let mut inputs = sid.as_bytes().iter().map(|c|(c-48)as i64);
+        let result = run(&monad, &mut inputs);
+        let valid = result.regs[3] == 0;
+        if (id % 100001) == 0{
+            println!("{}: {}", sid, valid);
+        }
+        if valid{
+            return id;
+        }
+        if id == 0{
+            break;
+        }
+    }*/
+    -1
+}
+
+#[aoc(day24, part2)]
+pub fn part2(input: &[u8]) -> i64 {
+    -1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part1, part2, run, parse_program};
+
+    #[test]
+    fn test_a(){
+        let prog = "inp x
+mul x -1";
+        let instrs = parse_program(prog);
+        let mut inputs = vec![4].into_iter();
+        let mut result = run(&instrs, &mut inputs);
+        assert_eq!(result.regs[1], -4);
+    }
+
+    #[test]
+    fn test_b(){
+        let prog = "inp z
+inp x
+mul z 3
+eql z x
+";
+        let instrs = parse_program(prog);
+        let mut inputs1 = vec![4,12].into_iter();
+        let mut inputs2 = vec![4,11].into_iter();
+        let result1 = run(&instrs, &mut inputs1);
+        let result2 = run(&instrs, &mut inputs2);
+        println!("{:?}", result1);
+        println!("{:?}", result2);
+        assert_eq!(result1.regs[3], 1);
+        assert_eq!(result2.regs[3], 0);
+    }
+
+
+
+    #[test]
+    fn test2(){
+        assert_eq!(part1(
+            ""), 19);
+    }
+}
+
+
+
